8020f92f7b3acd89cb3cb763a74d2b636f8d5022
Pedro Motta <pedromotta20@gmail.com>
2021-01-31 19:15:41 -0300

    command say

--- index.js
+++ index.js
@@ -22,4 +22,25 @@ const client = new Discord.Client();
 
 const config = require("./config.json");
 
+client.on("message", (message) => {
+  if (message.author.bot) return;
+  if (message.channel.type == "dm") return;
+  if (!message.content.toLowerCase().startsWith(config.prefix)) return;
+  if (
+    message.content.startsWith(`<@!${client.user.id}>`) ||
+    message.content.startsWith(`<@${client.user.id}>`)
+  )
+    return;
+
+  const args = message.content.trim().slice(config.prefix.length).split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  try {
+    const commandFile = require(`./commands/${command}.js`);
+    commandFile.run(client, message, args);
+  } catch (err) {
+    console.error("Erro: " + err);
+  }
+});
+
 client.login(token);
