b622697ff636870f1ae72b0c1f9106f004ec75fa
Pedro Motta <pedromotta20@gmail.com>
2021-01-31 21:28:36 -0300

    Update anime.js

--- commands/anime.js
+++ commands/anime.js
@@ -2,12 +2,14 @@ const Jikan = require("jikan-node");
 const mal = new Jikan();
 
 module.exports.run = async (client, message, args) => {
-  var anime = "";
+  var anime = "Anime não encontrado";
 
-  mal
-    .findAnime("11597", "episodes", 1)
-    .then((info) => (anime = info))
-    .catch((err) => (anime = err));
+  async function findAnime() {
+    mal
+      .findAnime("11597", "episodes", 1)
+      .then((info) => (anime = info))
+      .catch((err) => (anime = err));
+  }
 
   message.channel.send(anime);
 };
