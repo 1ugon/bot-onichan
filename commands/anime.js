91545442b3537b5828251abfaeb8efb51b8c5b74
Pedro Motta <pedromotta20@gmail.com>
2021-01-31 22:36:32 -0300

    running in dev enviroment

--- commands/anime.js
+++ commands/anime.js
@@ -4,6 +4,9 @@ const mal = new Jikan();
 module.exports.run = async (client, message, args) => {
   mal
     .findAnime("11597", "episodes", 1)
-    .then((info) => message.channel.send(info))
-    .catch((err) => message.channel.send(err));
+    .then(function (info) {
+      message.channel.send(info);
+    })
+    .catch((err) => console.log(err));
+  /* message.channel.send(findAnime()) */
 };
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
 
 const token = process.env.TOKEN;
 const port = process.env.PORT;
